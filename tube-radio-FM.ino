5d36525db0173f069f0d1343af6ab696055ee5a2
FilBip <plq@free.fr>
2019-11-08 17:58:03 +0100

    Add files via upload

--- tube-radio-FM.ino
+++ tube-radio-FM.ino
@@ -1,53 +1,64 @@
 /*
- Add an FM receiver to an old tube radio.
- Use a RDA5807M chip as receiver znd an Arduino Pro Mini as driver.
+ tube-radio-FM
+ Adds an FM receiver to an old tube radio.
+ The receiver is an RDA5807M module driven by an Arduino Pro Mini.
  The original variable capacitor in the radio is used for tunning.
- An external NE555 oscillator is used for varcap measuring..
+ An external NE555 oscillator is used for varcap measuring.
  Source pour l'oscillateur NE555 et la mesure de fréquences: http://www.f-legrand.fr/scidoc/docmml/sciphys/arduino/oscillateur/oscillateur.html
  
  Philippe Leclercq 2019
  */
+ 
 #define DEBUG 1
 
 #include <radio.h>
 #include <RDA5807M.h> // Radio library from https://github.com/mathertel/Radio
 
 RDA5807M radio;    // Create an instance of a RDA5807 chip radio
+int prevChan=0;
 
-RADIO_INFO info;
+// Min and max varcap values (pF)
+#define MINCAP 43
+#define MAXCAP 439
+
+// Convert pF boundaries to oscillator frequencies
+// NE555 oscillator freq-capacitance conversion: F = 1.44 / ( (R1+2R2) * C ) with R1=R2=1Mohm
+#define CONVERTFACTOR 480000 // (1.44/3)*10e6
+unsigned long lowBound = CONVERTFACTOR/MAXCAP;
+unsigned long freqBound = CONVERTFACTOR/MINCAP - CONVERTFACTOR/MAXCAP;
 
 
-char inputPin = 5; // oscillator input to Timer1
+#define INPUTPIN 5 // oscillator signal to pulse counter (Timer1)
  
 volatile uint16_t count_high,count_low;
 volatile uint32_t count;
 volatile uint16_t ic; // interrupts counter
-uint16_t intPerSample=10; // number of interrupts for a sample
-float deltaT;
+uint16_t nbPerSample=5; // number of periodic interrupts for a sample
+float deltaT; // Elapsed time per sample
                       
 void start_count() {
   // Timer2 : periodic interrupts
   TCCR2A |= (1 << WGM21); // CTC mode 'clear timer on compare), top = OCR2A
-  OCR2A = 0xFF; // fréquence d'interruption 16/8 MHz /1024/256
+  OCR2A = 0xFF; // periodic interrupt frequency 16or8 MHz /1024/256
   TIMSK2 = 1 << TOIE2; // overflow interrupt enable
   TCNT2 = 0;
   ic = 0;
  
-  // Timer1 : pulse counter
+  // Timer1 : pulse counter (on rising edge)
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1 = 0;
   TIMSK1 = 1<<TOIE1; // overflow interrupt enable
   count = 0;
  
-  sei(); // activation des interruptions
+  sei(); // enable interrupts
   TCCR2B |= (1 << CS12) | (1 << CS11) | (1 << CS10); // prescaler = 1024
   TCCR1B |= (1 << CS12) | (1 << CS11) | (1 << CS10); // external clock on rising edge
 }
                        
 ISR(TIMER2_OVF_vect) { // Overflow interrupt
    ic++;
-   if (ic==intPerSample) {
+   if (ic==nbPerSample) {
      ic = 0;
      count_low = TCNT1;
      TCNT1 = 0;
@@ -60,52 +71,32 @@ ISR(TIMER1_OVF_vect) {
     count_high++;
 }
 
-// Min and max varcap values (pF)
-#define MINCAP 41 
-#define MAXCAP 439
-
-// NE555 oscillator freq-capacitance conversion (R1=1Mohm R2=1Mohm)
-// F = 1.44 / ( (R1+2R2) * C )
-#define CONVERTFACTOR 480000
-unsigned long freqBound=CONVERTFACTOR/MINCAP-CONVERTFACTOR/MAXCAP;
-unsigned long lowBound=CONVERTFACTOR/MAXCAP;
-int prevChan=0;
-#define TICK 1000
-
-
 void setup() 
 {
 #if DEBUG
   Serial.begin(115200); 
-  Serial.println("");
-  Serial.print("Radio...");
+  Serial.println("\nRadio...");
 #endif
 
-   pinMode(inputPin,INPUT);
-  deltaT = 1.0*intPerSample*1024*256/F_CPU; // 1.0 necessary to compute as float
+  pinMode(INPUTPIN,INPUT);
+  deltaT = 1.0*nbPerSample*1024*256/F_CPU; // 1.0 necessary to compute as float
   start_count();
   
    // Initialize the Radio 
-  int s=radio.init();
-#if DEBUG
-  if (s)
-    Serial.println(" Found");
-  else
-    Serial.println(" Not found");
-#endif
+  radio.init();
   radio.setMono(true);
   radio.setVolume(2);
-  radio.setMute(true); 
 }
 
-void loop() {
- 
+void loop()
+{
   unsigned long freq = count/deltaT;
   if (freq < lowBound)
     freq=lowBound;
-  unsigned capa = CONVERTFACTOR/freq;
   unsigned long chan = 1080-(freq-lowBound)*210/freqBound;
+
 #if DEBUG
+  unsigned capa = CONVERTFACTOR/freq;
   Serial.print("Interrupts=");
   Serial.print(count);
   Serial.print(" Freq=");
@@ -116,19 +107,23 @@ void loop() {
   Serial.print(chan);
   Serial.println("");
 #endif
-     if (chan < 870) chan=870;
-     if (chan > 1080) chan=1080;
-      chan*=10; // to radio library format
-      if (chan != prevChan) {
-           prevChan = chan;
+
+  if (chan < 870) chan=870;
+  if (chan > 1080) chan=1080;
+  chan*=10; // to radio library format
+  if (chan != prevChan) {
+     prevChan = chan;
+
 #if DEBUG
-             Serial.print (" Tuned to ");
-             Serial.println (chan);
+     Serial.print (" Tuned to ");
+     Serial.println (chan);
 #endif
-         radio.setFrequency(chan);
-      }
-    delay (100); // pause!
+
+     radio.setFrequency(chan);
+  }
+  delay (100); // pause!
+
 #if DEBUG
-   delay (1000);
+  delay (1900);  // slow debug :)
 #endif
 }
